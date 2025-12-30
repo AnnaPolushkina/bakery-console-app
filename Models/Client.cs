c6a804a0055a62e42fc1babb2a1edbca6c7d03e0
Anna <annapolushkina13@gmail.com>
2025-12-30 03:20:26 +0100

    Add Client and Order models

---
+++ Models/Client.cs
@@ -0,0 +1,8 @@
+namespace BakeryConsoleApp.Models;
+
+public class Client
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string PhoneNumber { get; set; }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+namespace BakeryConsoleApp.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int ClientId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalPrice { get; set; }
+}
